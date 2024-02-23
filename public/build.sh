f45d955f50f08509a9521bb3b17771ead87de462
The Maximalist <hello@themaximalist.com>
2024-02-23 00:37:17 -0800

    spacing

--- public/build.sh
+++ public/build.sh
@@ -4,6 +4,6 @@ NAME="scrape.js"
 DOMAIN="scrapejs.themaximalist.com"
 TITLE="Scrape.js — Web Scraping Library for Node.js"
 DESCRIPTION="Simple but feature-packed web scraping library for Node.js."
-CLASSES="gap-sm"
+CLASSES="gap-sm-2"
 
 readme-to-index "$NAME" "$DOMAIN" "$TITLE" "$DESCRIPTION" "$CLASSES"
